4a70d1d4961f94a08a27435dacb5fc297de5d152
mahmood2122 <mahmoodsino40@gmail.com>
2024-04-17 20:31:36 +0400

    pokemon

--- src/components/layout/home/HomePage.test.js
+++ src/components/layout/home/HomePage.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { HomePage } from './index';
 
-// Mock the CategoriesContext module
 jest.mock('@/context/auth/CategoriesContext', () => ({
   useCategories: jest.fn(() => ({
     categories: {
@@ -16,14 +15,10 @@ describe('HomePage', () => {
   it('renders the homepage with categories', () => {
     render(<HomePage />);
     
-    // Check if the title is rendered
     expect(screen.getByText('ALL CATEGORIES')).toBeInTheDocument();
 
-    // Check if each category is rendered as a link
     expect(screen.getByText('Category 1')).toHaveAttribute('href', './category1');
     expect(screen.getByText('Category 2')).toHaveAttribute('href', './category2');
-
-    // Check if the Search component is rendered
     expect(screen.getByTestId('search-component')).toBeInTheDocument();
   });
 });
